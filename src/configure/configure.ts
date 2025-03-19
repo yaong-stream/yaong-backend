213c03c18b0751f7408dddf045a76ea051e5f4a0
narumir <narumir@narumir.io>
2025-03-19 07:04:27 +0000

    add validator package and apply swagger

--- src/configure/configure.ts
+++ src/configure/configure.ts
@@ -4,7 +4,9 @@ import {
 
 const env = () => ({
   port: parseInt(process.env.PORT || "4000"),
+  isProduction: process.env.NODE_ENV === "production",
 });
+
 export const Configure = ConfigModule.forRoot({
   load: [
     env,
