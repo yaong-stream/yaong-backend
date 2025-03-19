bfd6c7afdf9b7e3c0d9c54ae5b4cdcf88712c7a4
narumir <narumir@narumir.io>
2025-03-19 07:56:56 +0000

    add typeorm datasource and health checker

--- src/configure/configure.ts
+++ src/configure/configure.ts
@@ -3,8 +3,14 @@ import {
 } from "@nestjs/config";
 
 const env = () => ({
-  port: parseInt(process.env.PORT || "4000"),
+  port: parseInt(process.env.PORT || "4000", 10),
   isProduction: process.env.NODE_ENV === "production",
+  database: {
+    host: process.env.DATABASE_HOST,
+    port: parseInt(process.env.DATABASE_PORT || "5432", 10),
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+  },
 });
 
 export const Configure = ConfigModule.forRoot({
---
+++ src/configure/database.ts
@@ -0,0 +1,36 @@
+import {
+  ConfigModule,
+  ConfigService,
+} from "@nestjs/config";
+import {
+  TypeOrmModule,
+} from "@nestjs/typeorm";
+import {
+  EntitySchema,
+  MixedList,
+} from "typeorm";
+
+const entities: MixedList<string | Function | EntitySchema<any>> = [
+
+];
+
+export const PostgresDataSource = TypeOrmModule.forRootAsync({
+  imports: [
+    ConfigModule,
+  ],
+  inject: [
+    ConfigService,
+  ],
+  useFactory: (configService: ConfigService) => ({
+    type: "postgres",
+    database: "yaong",
+    schema: "public",
+    host: configService.getOrThrow<string>("database.host"),
+    port: configService.getOrThrow<number>("database.port"),
+    username: configService.getOrThrow<string>("database.username"),
+    password: configService.getOrThrow<string>("database.password"),
+    synchronize: !configService.getOrThrow<boolean>("isProduction"),
+    logging: !configService.getOrThrow<boolean>("isProduction"),
+    entities,
+  }),
+});
