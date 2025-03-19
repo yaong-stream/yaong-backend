b2af1b7c237e8308b6944f30e65287fa829eddba
narumir <narumir@narumir.io>
2025-03-19 06:53:47 +0000

    apply config service, cookie-parser and helmt

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
 import {
   Module,
 } from '@nestjs/common';
+import {
+  Configure,
+} from './configure';
 
 @Module({
-  imports: [],
+  imports: [
+    Configure,
+  ],
 })
 export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,35 @@
+import * as cookieParser from "cookie-parser";
+import helmet from "helmet";
 import {
   NestFactory,
 } from '@nestjs/core';
+import {
+  ConfigService,
+} from '@nestjs/config';
+import {
+  ConsoleLogger,
+} from '@nestjs/common';
+import {
+  NestExpressApplication,
+} from '@nestjs/platform-express';
 import {
   AppModule,
 } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    logger: new ConsoleLogger({ json: true, colors: process.env.NODE_ENV !== "production" }),
+  });
+  const configService = app.get(ConfigService);
+  const port = configService.getOrThrow<number>('port');
+
+  app
+    .set("query parser", "extended")
+    .set("trust proxy", true);
+  app
+    .enableShutdownHooks()
+    .use(cookieParser())
+    .use(helmet());
+  await app.listen(port);
 }
 bootstrap();
